bcfddcb7e4ced6bcb047b3b7b5842a09d7dd0dab
classeverte <34238933+classeverte@users.noreply.github.com>
2017-12-04 21:01:38 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,17 @@
+<nav>
+
+				<ul>
+
+					<li><a href="Cocouvpage1.html">Le contexte: Chiffres & Problèmes</a></li>
+
+					<li><a href="Cocouvpage1.html">Notre Solution: le Projet Cocouv'</a></li>
+
+					<li><a href="Cocouvpage1.html">Les Bénéfices</a></li>
+
+					<li><a href="Cocouvpage1.html">Qui sommes nous ?</a></li>
+
+					<li><a href="Cocouvpage2.html">Contact & Presse</a></li>
+
+				</ul>
+
+			</nav>
